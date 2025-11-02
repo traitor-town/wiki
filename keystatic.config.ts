f1ab4d7da8e91fe97f1e37c186ac525eebacaeb8
Gurkan <hello@gurkz.me>
2025-11-02 13:13:27 +0100

    add authors to keystatic

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,29 +1,57 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: 'github',
-    repo: "traitor-town/wiki"
+    kind: "github",
+    repo: "traitor-town/wiki",
   },
   collections: {
     maps: collection({
-      label: 'Maps',
-      slugField: 'title',
-      path: 'src/content/maps/*',
-      format: { contentField: 'content' },
+      label: "Maps",
+      slugField: "title",
+      path: "src/content/maps/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         creators: fields.text({ label: "creators" }),
         content: fields.markdoc({
-          label: 'Content',
+          label: "Content",
           options: {
             image: {
-              directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
+              directory: "src/assets/images/posts",
+              publicPath: "../../assets/images/posts/",
             },
           },
         }),
+
+        authors: fields.array(
+          fields.relationship({
+            label: "Authors",
+            collection: "authors",
+            validation: {
+              isRequired: true,
+            },
+          }),
+          {
+            label: "Authors",
+            itemLabel: (item) => item.value || "please select an author",
+          }
+        ),
       },
+    }),
+    authors: collection({
+      label: "Authors",
+      slugField: "name",
+      path: "src/content/authors/*",
+      schema: {
+        name: fields.slug({ name: { label: "Name" } }),
+        avatar: fields.image({
+          label: "Avatar",
+          directory: "src/assets/images/avatars/*",
+          publicPath: "@assets/images/avatars/",
+        }),
+      },
+      format: "json",
     }),
   },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,18 @@
 // @ts-ignore
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const maps = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    creators: z.string()
+    creators: z.string(),
+    authors: z.array(
+      z.object({
+        name: z.string(),
+        slug: z.string(),
+        avatar: z.string(),
+      })
+    ),
   }),
 });
 
